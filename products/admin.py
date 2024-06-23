e9f49cbd691da78fb430e07eca7b4a24b34bcb9f
Mamali52 <mamadjavad1382s21@gmail.com>
2024-06-23 13:57:26 -0700

    create products model

---
+++ products/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from products.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price',)
+
