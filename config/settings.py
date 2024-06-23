0ca462206666c064d10243ee5b7d55f7f2121391
Mamali52 <mamadjavad1382s21@gmail.com>
2024-06-23 10:36:15 -0700

    change some accounts templates

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,12 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'mamadjavad1382s21@gmail.com'
+# EMAIL_HOST_PASSWORD = 'Mamadjavad-5733'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
