1c6c7ba1eac52718472632cacdf2e1eabbed89fd
Mamali52 <mamadjavad1382s21@gmail.com>
2024-06-27 09:05:52 -0700

    add translation product app

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, Comment
+
+
+class CommentsInline(admin.TabularInline):
+    model = Comment
+    fields = ['body', 'author', 'stars', 'active']
+    extra = 1
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active',]
+
+    inlines = [CommentsInline]
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['product', 'body', 'author', 'stars', 'active']
 
--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+
 from django.shortcuts import reverse
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 
 class Product(models.Model):
@@ -19,14 +21,41 @@ class Product(models.Model):
         return reverse('product_detail', args=[self.pk])
 
 
+class ActiveCommentsManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentsManager, self).get_queryset().filter(active=True)
+
+
 class Comment(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-    text = models.TextField()
-    Product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
-    is_active = models.BooleanField(default=True)
-    recommend = models.BooleanField(default=True)
+    PRODUCT_STARS = [
+        ('1', _('very bad')),
+        ('2', _('bad')),
+        ('3', _('normal')),
+        ('4', _('good')),
+        ('5', _('perfect')),
+    ]
+
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='comments',
+        verbose_name=_('Comment Author')
+    )
+
+    body = models.TextField(verbose_name=_('comment text'))
+
+    stars = models.CharField(max_length=10, choices=PRODUCT_STARS, verbose_name=_('Score'))
 
     datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.text
+    active = models.BooleanField(default=True)
+
+    # Manager
+    objects = models.Manager()
+    active_comments_manager = ActiveCommentsManager()
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.product.id])
